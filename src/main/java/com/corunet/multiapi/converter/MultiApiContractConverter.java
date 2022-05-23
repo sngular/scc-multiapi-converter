6910460a8174f50326d4bc7527f7b7c9b6222dd3
Pablo José López Rivadulla <59642284+Rivarsal@users.noreply.github.com>
2022-05-23 13:58:57 +0200

    Added pom dependencies and Style

--- src/main/java/com/corunet/multiapi/converter/MultiApiContractConverter.java
+++ src/main/java/com/corunet/multiapi/converter/MultiApiContractConverter.java
@@ -1,4 +1,27 @@
 package com.corunet.multiapi.converter;
 
-public class MultiApiContractConverter {
+import java.io.File;
+import java.util.Collection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.contract.spec.Contract;
+import org.springframework.cloud.contract.spec.ContractConverter;
+
+@Slf4j
+public class MultiApiContractConverter implements ContractConverter<Collection<Contract>> {
+
+  @Override
+  public boolean isAccepted(final File file) {
+    return false;
+  }
+
+  @Override
+  public Collection<Contract> convertFrom(final File file) {
+    return null;
+  }
+
+  @Override
+  public Collection<Contract> convertTo(final Collection<Contract> contract) {
+    return null;
+  }
 }
