1251577e74c61b6044d9049842ef60e4260cb03a
Rivarsal <59642284+Rivarsal@users.noreply.github.com>
2022-06-01 17:36:55 +0200

    Test name change for maven central (#26)
    * Test Name Change
    
    * #25 Upped pom version

--- src/test/java/net/coru/multiapi/converter/openapi/OpenApiContractConverterUtilsTest.java
+++ src/test/java/net/coru/multiapi/converter/openapi/OpenApiContractConverterUtilsTest.java
@@ -4,7 +4,7 @@
  *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-package com.corunet.multiapi.converter.openapi;
+package net.coru.multiapi.converter.openapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
