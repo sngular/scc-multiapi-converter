7bed4327b3b13752f04f1125cc338bce4854eb56
Rivarsal <59642284+Rivarsal@users.noreply.github.com>
2022-07-15 12:27:52 +0200

    #56 Changed package from public to protected (#57)

--- src/test/java/net/coru/multiapi/converter/asyncapi/AsyncApiContractConverterTestFixtures.java
+++ src/test/java/net/coru/multiapi/converter/asyncapi/AsyncApiContractConverterTestFixtures.java
@@ -12,89 +12,89 @@ import java.util.Map;
 
 public class AsyncApiContractConverterTestFixtures {
 
-  public final String EVENT_API_FILE = "src/test/resources/asyncapi/event-api.yml";
+  protected final String EVENT_API_FILE = "src/test/resources/asyncapi/event-api.yml";
 
-  public final String TEST_BASIC_TYPES_FILE = "src/test/resources/asyncapi/testBasicTypes.yml";
+  protected final String TEST_BASIC_TYPES_FILE = "src/test/resources/asyncapi/testBasicTypes.yml";
 
-  public final String TEST_COMPLEX_OBJECTS_FILE = "src/test/resources/asyncapi/testComplexObjects.yml";
+  protected final String TEST_COMPLEX_OBJECTS_FILE = "src/test/resources/asyncapi/testComplexObjects.yml";
 
-  public final String TEST_ARRAYS_FILE = "src/test/resources/asyncapi/testArrays.yml";
+  protected final String TEST_ARRAYS_FILE = "src/test/resources/asyncapi/testArrays.yml";
 
-  public final String TEST_ARRAYS_REF_FILE = "src/test/resources/asyncapi/testArraysWithRef.yml";
+  protected final String TEST_ARRAYS_REF_FILE = "src/test/resources/asyncapi/testArraysWithRef.yml";
 
-  public final String TEST_ENUMS_FILE = "src/test/resources/asyncapi/testEnums.yml";
+  protected final String TEST_ENUMS_FILE = "src/test/resources/asyncapi/testEnums.yml";
 
-  public final String TEST_EXTERNAL_FILE = "src/test/resources/asyncapi/testExternalFiles.yml";
+  protected final String TEST_EXTERNAL_FILE = "src/test/resources/asyncapi/testExternalFiles.yml";
 
-  public final String TEST_EXTERNAL_FILE_MULTIPLE_SCHEMAS = "src/test/resources/asyncapi/testExternalFilesWithMultipleSchemas.yml";
+  protected final String TEST_EXTERNAL_FILE_MULTIPLE_SCHEMAS = "src/test/resources/asyncapi/testExternalFilesWithMultipleSchemas.yml";
 
-  public final String PUBLISH_NAME = "publishOperation";
+  protected final String PUBLISH_NAME = "publishOperation";
 
-  public final String TRIGGERED_BY = "publishOperation()";
+  protected final String TRIGGERED_BY = "publishOperation()";
 
-  public final String PUBLISH_SEND_TO = "orderCreated";
+  protected final String PUBLISH_SEND_TO = "orderCreated";
 
-  public final String SUBSCRIBE_NAME = "subscribeOperation";
+  protected final String SUBSCRIBE_NAME = "subscribeOperation";
 
-  public final String MESSAGE_FROM = "createOrder";
+  protected final String MESSAGE_FROM = "createOrder";
 
-  public final String INT_ARRAY_BODY_MATCHER = "order.intArray[0]";
+  protected final String INT_ARRAY_BODY_MATCHER = "order.intArray[0]";
 
-  public final String HEADER_NAME = "Accept";
+  protected final String HEADER_NAME = "Accept";
 
-  public final String HEADER_VALUE = "application/json";
+  protected final String HEADER_VALUE = "application/json";
 
-  public final String ORDER = "order";
+  protected final String ORDER = "order";
 
-  public final String ORDERS = "orders";
+  protected final String ORDERS = "orders";
 
-  public final String ORDER_LINE = "orderLine";
+  protected final String ORDER_LINE = "orderLine";
 
-  public final String EMPLOYEES = "employees";
+  protected final String EMPLOYEES = "employees";
 
-  public final String INT_ARRAY = "intArray";
+  protected final String INT_ARRAY = "intArray";
 
-  public final String NAME = "name";
+  protected final String NAME = "name";
 
-  public final String CORUNET = "Corunet";
+  protected final String CORUNET = "Corunet";
 
-  public final String PERSON_NAME = "Carlos";
+  protected final String PERSON_NAME = "Carlos";
 
-  public final String ADDRESS = "address";
+  protected final String ADDRESS = "address";
 
-  public final String COMPANY_NAME = "companyName";
+  protected final String COMPANY_NAME = "companyName";
 
-  public final String REFERENCE_NAME = "referenceName";
+  protected final String REFERENCE_NAME = "referenceName";
 
-  public final String STREET = "street";
+  protected final String STREET = "street";
 
-  public final String STREET_VALUE = "Calle Sor Joaquina";
+  protected final String STREET_VALUE = "Calle Sor Joaquina";
 
-  public final String AMOUNT = "amount";
+  protected final String AMOUNT = "amount";
 
-  public final String INTEGER_TYPE = "integerType";
+  protected final String INTEGER_TYPE = "integerType";
 
-  public final String INTEGER_TYPE_2 = "integerType2";
+  protected final String INTEGER_TYPE_2 = "integerType2";
 
-  public final String FLOAT_TYPE = "floatType";
+  protected final String FLOAT_TYPE = "floatType";
 
-  public final String FLOAT_TYPE_2 = "floatType2";
+  protected final String FLOAT_TYPE_2 = "floatType2";
 
-  public final String DOUBLE_TYPE = "doubleType";
+  protected final String DOUBLE_TYPE = "doubleType";
 
-  public final String STRING_TYPE = "stringType";
+  protected final String STRING_TYPE = "stringType";
 
-  public final String BOOLEAN_TYPE = "booleanType";
+  protected final String BOOLEAN_TYPE = "booleanType";
 
-  public final String IS_SENT = "isSent";
+  protected final String IS_SENT = "isSent";
 
-  public final List<Integer> INT_ARRAY_VALUES = List.of(1, 2, 3);
+  protected final List<Integer> INT_ARRAY_VALUES = List.of(1, 2, 3);
 
-  public final List<Double> DOUBLE_ARRAY_VALUES = List.of(1.25, 2.5, 3.75);
+  protected final List<Double> DOUBLE_ARRAY_VALUES = List.of(1.25, 2.5, 3.75);
 
-  public final String[] ENUM_VALUES = {"Corunet", "Sngular"};
+  protected final String[] ENUM_VALUES = {"Corunet", "Sngular"};
 
-  public final Map<String, Object> createOrder() {
+  protected final Map<String, Object> createOrder() {
     Map<String, Object> order = new HashMap<>();
     Map<String, Object> array = new HashMap<>();
     array.put(INT_ARRAY, INT_ARRAY_VALUES);
